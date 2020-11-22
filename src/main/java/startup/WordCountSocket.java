26b5528f2487d4dbfb098ec59929937d7c1a475f
ErCHen <940909850@qq.com>
2020-11-22 22:31:50 +0800

    word count

---
+++ src/main/java/startup/WordCountSocket.java
@@ -0,0 +1,27 @@
+package startup;
+
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+import org.apache.flink.api.java.tuple.Tuple2;
+
+public class WordCountSocket {
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        DataStream<String> stream = env.socketTextStream("localhost", 9999);
+        stream.flatMap(new Tokenizer()).keyBy(r -> r.f0).sum(1).print();
+        env.execute("wc-java");
+    }
+
+    public static class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> collector) {
+            for (String str: value.split(" ")) {
+                collector.collect(new Tuple2<>(str, 1));
+            }
+        }
+    }
+}
